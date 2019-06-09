eaab4e5b6e6e3f287a4046f2027f0455b6f889d3
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-06-09 17:28:07 -0500

    Clase 2 - Variables: Strings

---
+++ Clase2.js
@@ -0,0 +1,13 @@
+var nombre = 'Diego', apellido = 'Martínez'
+
+var nombreEnMayusculas = nombre.toUpperCase()
+var apellidoEnMinusculas = apellido.toLowerCase()
+
+var primeraLetraNombre = nombre.charAt(0)
+var cantidadLetrasNombre = nombre.length
+
+var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
+
+var str = nombre.substr(1, 2)
+
+var ultimaLetraNombre = nombre.charAt(nombre.length-1)
