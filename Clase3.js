e98ece0f0e5f85034cc3a24b9dc030ab7494eb1f
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-06-09 17:44:40 -0500

    Clase 3 - Variables: Numeros

---
+++ Clase3.js
@@ -0,0 +1,23 @@
+var edad = 21
+//edad = edad + 1
+edad += 1
+
+var peso = 60
+//peso = peso - 2
+peso -= 2
+
+var sandwitch = 1
+peso += sandwitch
+
+var jugarFutbol = 3
+peso -= jugarFutbol
+
+var precioDeVino = 200.3
+
+var total = Math.round(precioDeVino * 100 * 3) /100
+var totalStr = total.toFixed(2)
+var total2 = parseFloat(totalStr)
+
+var pizza = 8
+var personas = 2
+var porcion = pizza / personas
