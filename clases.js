7398092dbdc61d9fb9a7c9bc2a99969880b20d1e
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-06-13 15:30:08 -0500

    La verdad oculta sobre las clases en JavaScript
    Los objetos en JavaScript son “contenedores” dinámicos de propiedades. Estos objetos poseen un enlace a un objeto prototipo. Cuando intentamos acceder a la propiedad de un objeto, la propiedad no sólo se busca en el propio objeto sino también en el prototipo del objeto, en el prototipo del prototipo, y así sucesivamente hasta que se encuentre una propiedad que coincida con el nombre o se alcance el final de la cadena de prototipos.

--- clases.js
+++ clases.js
@@ -1,3 +1,10 @@
+function heredaDe(prototipoHijo, prototipoPadre) {
+    var fn = function () {}
+    fn.prototype = prototipoPadre.prototype
+    prototipoHijo.prototype = new fn
+    prototipoHijo.prototype.constructor = prototipoHijo
+}
+
 function Persona(nombre, apellido, altura) {
     this.nombre = nombre
     this.apellido = apellido
@@ -8,14 +15,24 @@ Persona.prototype.saludar = function () {
     console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
 }
 
-const ALTO = 1.8
-
 Persona.prototype.soyAlto = function () {
-    if (this.altura >= ALTO) {
-        return console.log(`Soy alto!`)
-    }
+    return this.altura >= 1.8
 }
 
+function Desarrollador(nombre, apellido) {
+    this.nombre = nombre
+    this.apellido = apellido
+}
+
+heredaDe(Desarrollador, Persona)
+
+Desarrollador.prototype.saludar = function () {
+    console.log(`Hola me llamo ${this.nombre} ${this.apellido} y soy desarrollador`)
+}
+
+/* 
 var sacha = new Persona('Sacha', 'Lifszyc', 1.85)
 var erika = new Persona('Erika', 'Luna', 1.70)
 var arturo = new Persona('Arturo', 'Gil', 1.82)
+ */
+
