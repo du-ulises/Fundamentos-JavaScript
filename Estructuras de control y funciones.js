45bb32dbe520bab388323f5288681f078a4e08e3
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-06-13 12:39:09 -0500

    Estructuras repetitivas: while

--- Estructuras de control y funciones.js
+++ Estructuras de control y funciones.js
@@ -100,18 +100,37 @@ console.log(`Al inicio del año ${diego.nombre} pesa ${diego.peso}kg`)
 /* function aumentarDePeso(prsona) {
     return persona.peso += peso
 } */
-const INCREMENTO_PESO = .2
-const DIAS_DEL_ANIO =365
+const INCREMENTO_PESO = .3
+const DIAS_DEL_ANIO = 365
+
 const aumentarDePeso = persona => persona.peso += INCREMENTO_PESO
 const adelgazar = persona => persona.peso -= INCREMENTO_PESO
 
-for (var i = 1; i <= DIAS_DEL_ANIO; i++) {
+/* for (var i = 1; i <= DIAS_DEL_ANIO; i++) {
     var random = Math.random()
     if (random < 0.25) {
         aumentarDePeso(diego)
     } else if (random < 0.5) {
         adelgazar(diego)
     }
+} */
+
+/* console.log(`Al final del año ${diego.nombre} pesa ${diego.peso.toFixed(1)}kg`)
+ */
+const comeMucho = () => Math.random() < 0.3
+const realizarDeporte = () => Math.random() < 0.4
+
+const META = diego.peso - 3
+var dias = 0
+
+while (diego.peso > META) {
+    if (comeMucho()) {
+        aumentarDePeso(diego)
+    }
+    if (realizarDeporte()) {
+        adelgazar(diego)
+    }
+    dias += 1
 }
 
-console.log(`Al final del año ${diego.nombre} pesa ${diego.peso.toFixed(1)}kg`)
+console.log(`Pasaron ${dias} días hasta que ${diego.nombre} adelgazó 3kg`)
