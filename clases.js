148d287c32d7863290694586577e5f11a3cdad2e
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-06-13 17:26:32 -0500

    Clases en JavaScript
    Las clases de JavaScript son introducidas en el ECMAScript 2015 y son una mejora en la sintaxis sobre la herencia basada en prototipos de JavaScript.
    
    La palabra clave extends se usa en declaraciones de clase o expresiones de clase para crear una clase que es hija de otra clase.
    
    El método constructor es un método especial para crear e inicializar un objeto creado a partir de una clase.

--- clases.js
+++ clases.js
@@ -1,38 +1,35 @@
-function heredaDe(prototipoHijo, prototipoPadre) {
+/* function heredaDe(prototipoHijo, prototipoPadre) {
     var fn = function () {}
     fn.prototype = prototipoPadre.prototype
     prototipoHijo.prototype = new fn
     prototipoHijo.prototype.constructor = prototipoHijo
+} */
+
+class Persona {
+    constructor(nombre, apellido, altura) {
+        this.nombre = nombre
+        this.apellido = apellido
+        this.altura = altura
+    }
+    saludar() {
+        console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
+    }
+    soyAlto() {
+        return this.altura >= 1.8
+    }
 }
 
-function Persona(nombre, apellido, altura) {
-    this.nombre = nombre
-    this.apellido = apellido
-    this.altura = altura
+class Desarrollador extends Persona {
+    constructor(nombre, apellido, altura) {
+        super(nombre, apellido, altura)
+    }
+    saludar() {
+        console.log(`Hola me llamo ${this.nombre} ${this.apellido} y soy desarrollador`)
+    }
 }
 
-Persona.prototype.saludar = function () {
-    console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
-}
-
-Persona.prototype.soyAlto = function () {
-    return this.altura >= 1.8
-}
-
-function Desarrollador(nombre, apellido) {
-    this.nombre = nombre
-    this.apellido = apellido
-}
-
-heredaDe(Desarrollador, Persona)
-
-Desarrollador.prototype.saludar = function () {
-    console.log(`Hola me llamo ${this.nombre} ${this.apellido} y soy desarrollador`)
-}
-
-/* 
+/*
 var sacha = new Persona('Sacha', 'Lifszyc', 1.85)
 var erika = new Persona('Erika', 'Luna', 1.70)
 var arturo = new Persona('Arturo', 'Gil', 1.82)
  */
-
