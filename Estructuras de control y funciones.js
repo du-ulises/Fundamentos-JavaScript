e78b54cc502ae5b900ed2bff4eeb890ce340617e
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-06-13 12:19:30 -0500

    Estructuras repetitivas: for

--- Estructuras de control y funciones.js
+++ Estructuras de control y funciones.js
@@ -41,7 +41,7 @@ function imprimirProfesiones(persona) {
     }
 }
 
-imprimirProfesiones(sacha)
+//imprimirProfesiones(sacha)
 
 const MAYORIA_DE_EDAD = 18
 
@@ -83,7 +83,35 @@ function permitirAcceso(persona) {
 const esMenorDeEdad = ({ edad }) => edad < MAYORIA_DE_EDAD
 
 function imprimirEsMenorDeEdad(persona) {
-    if(!esMenorDeEdad(persona)){
+    if (!esMenorDeEdad(persona)) {
         console.log(`No, ${persona.nombre} es mayor de edad`)
     }
 }
+
+var diego = {
+    nombre: 'Diego',
+    apellido: 'Martinez',
+    edad: 21,
+    peso: 60
+}
+
+console.log(`Al inicio del año ${diego.nombre} pesa ${diego.peso}kg`)
+
+/* function aumentarDePeso(prsona) {
+    return persona.peso += peso
+} */
+const INCREMENTO_PESO = .2
+const DIAS_DEL_ANIO =365
+const aumentarDePeso = persona => persona.peso += INCREMENTO_PESO
+const adelgazar = persona => persona.peso -= INCREMENTO_PESO
+
+for (var i = 1; i <= DIAS_DEL_ANIO; i++) {
+    var random = Math.random()
+    if (random < 0.25) {
+        aumentarDePeso(diego)
+    } else if (random < 0.5) {
+        adelgazar(diego)
+    }
+}
+
+console.log(`Al final del año ${diego.nombre} pesa ${diego.peso.toFixed(1)}kg`)
