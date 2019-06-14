ccbfa9fd0b5ca2a0260c40fed2657de705d9ac89
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-06-13 21:38:36 -0500

    Haciendo múltiples requests
    En esta clase accederemos a múltiples datos al mismo tiempo. Continuaremos trabajando con los jQuery y swapi.

--- callbacks.js
+++ callbacks.js
@@ -2,12 +2,15 @@ const API_URL = 'https://swapi.co/api/'
 
 const PEOPLE_URL = 'people/:id'
 
-const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`
-
 const opts = { croosDomain: true }
 
 const onPeopleResponse = function (persona) {
-    console.log(`Hola yo soy, ${persona.name}`)
+    console.log(`Hola, yo soy ${persona.name}`)
 }
 
-$.get(lukeUrl, opts, onPeopleResponse)
+function obtenerPersonaje(id) {
+    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+    $.get(url, opts, onPeopleResponse)
+}
+
+obtenerPersonaje(10)
