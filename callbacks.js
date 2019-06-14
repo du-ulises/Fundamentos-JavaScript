52da44fc3996d189da9840be5376bed5d3c458a0
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-06-13 21:56:32 -0500

    Manejando el Orden y el Asincronismo en JavaScript
    Una manera de asegurar que se respete la secuencia en que hemos realizado múltiples tareas es utilizando callbacks, con lo que se ejecutará luego, en cada llamada. Lo importante es que el llamado al callback se haga a través de una función anónima. Sin embargo, al hacerlo de esta manera generamos una situación poco deseada llamada CallbackHell.

--- callbacks.js
+++ callbacks.js
@@ -4,13 +4,24 @@ const PEOPLE_URL = 'people/:id'
 
 const opts = { croosDomain: true }
 
-const onPeopleResponse = function (persona) {
-    console.log(`Hola, yo soy ${persona.name}`)
-}
-
-function obtenerPersonaje(id) {
+function obtenerPersonaje(id, callback) {
     const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
-    $.get(url, opts, onPeopleResponse)
+
+    $.get(url, opts, function (persona) {
+        console.log(`Hola, yo soy ${persona.name}`)
+
+        if (callback) {
+            callback()
+        }
+    })
 }
 
-obtenerPersonaje(10)
+obtenerPersonaje(1, function () {
+    obtenerPersonaje(2, function () {
+        obtenerPersonaje(3, function () {
+            obtenerPersonaje(4, function () {
+                obtenerPersonaje(5)
+            })
+        })
+    })
+})
