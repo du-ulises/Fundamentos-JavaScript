8af72436e2174722b464bf6cc4b2a0779a613325
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-06-19 01:14:45 -0500

    Async-await: lo último en asincronismo
    Async-await es la manera más simple y clara de realizar tareas asíncronas. Await detiene la ejecución del programa hasta que todas las promesas sean resueltas. Para poder utilizar esta forma, hay que colocar async antes de la definición de la función, y encerrar el llamado a Promises.all() dentro de un bloque try … catch.

--- callbacks y promesas.js
+++ callbacks y promesas.js
@@ -19,16 +19,24 @@ function onError(id) {
     console.log(`Sucedio un error al obtener el personaje ${id}`)
 }
 
-var ids = [1, 2, 3, 4, 5, 6, 7]
 /* var promesas = ids.map(function (id) {
     return obtenerPersonaje(id)
 })
  */
-var promesas = ids.map(id => obtenerPersonaje(id))
-Promise
-    .all(promesas)
-    .then(personajes => console.log(personajes))
-    .catch(onError)
+
+async function obtenerPersonajes() {
+    var ids = [1, 2, 3, 4, 5, 6, 7]
+    var promesas = ids.map(id => obtenerPersonaje(id))
+    try {
+        var personajes = await Promise.all(promesas)
+        console.log(personajes)
+    } catch (id) {
+        onError(id)
+    }
+ }
+
+ obtenerPersonajes()
+
 /* 
 obtenerPersonaje(1)
     .then(personaje1 => {
