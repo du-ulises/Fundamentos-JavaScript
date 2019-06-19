cb9a88b6665f74d5895919a21c5699454d479a42
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-06-18 23:47:08 -0500

    Promesas
    En esta clase veremos las promesas, que son valores que aun no conocemos. Las promesas tienen tre estados:
    
    pending
    fullfilled
    rejected

--- callbacks.js
+++ callbacks.js
@@ -4,32 +4,23 @@ const PEOPLE_URL = 'people/:id'
 
 const opts = { croosDomain: true }
 
-function obtenerPersonaje(id, callback) {
+function obtenerPersonaje(id) {
+return new Promise((resolve, reject) => {
     const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
-
     $
-        .get(url, opts, callback)
-        .fail(() => {
-        console.log(`Sucedió un error. No se pudo obtener el personaje ${id}`)
-    })
+        .get(url, opts, function (data) {
+            resolve(data)
+        })
+        .fail(() => reject(id))
+})
 }
 
-obtenerPersonaje(1, function (personaje) {
-    console.log(`Hola, yo soy ${personaje.name}`)
+function onError(id) {
+    console.log(`Sucedio un error al obtener el personaje ${id}`)
+}
 
-    obtenerPersonaje(2, function (personaje) {
-        console.log(`Hola, yo soy ${personaje.name}`)
-
-        obtenerPersonaje(3, function (personaje) {
-            console.log(`Hola, yo soy ${personaje.name}`)
-
-            obtenerPersonaje(4, function (personaje) {
-                console.log(`Hola, yo soy ${personaje.name}`)
-
-                obtenerPersonaje(5, function (personaje) {
-                    console.log(`Hola, yo soy ${personaje.name}`)
-                })
-            })
-        })
+obtenerPersonaje(1)
+    .then(function (personaje) {
+        console.log(`El personaje 1 es ${personaje.name}`)
     })
-})
+    .catch(onError)
