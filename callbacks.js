e352c589b1979e752ae71abe29dfb1098317c968
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-06-18 23:15:19 -0500

    Manejo de errores con callbacks
    Para solucionar el problema de quedarnos sin conexión, u otro error similar, en medio de una sucesión de callbacks utilizamos el método fail().

--- callbacks.js
+++ callbacks.js
@@ -7,20 +7,28 @@ const opts = { croosDomain: true }
 function obtenerPersonaje(id, callback) {
     const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
 
-    $.get(url, opts, function (persona) {
-        console.log(`Hola, yo soy ${persona.name}`)
-
-        if (callback) {
-            callback()
-        }
+    $
+        .get(url, opts, callback)
+        .fail(() => {
+        console.log(`Sucedió un error. No se pudo obtener el personaje ${id}`)
     })
 }
 
-obtenerPersonaje(1, function () {
-    obtenerPersonaje(2, function () {
-        obtenerPersonaje(3, function () {
-            obtenerPersonaje(4, function () {
-                obtenerPersonaje(5)
+obtenerPersonaje(1, function (personaje) {
+    console.log(`Hola, yo soy ${personaje.name}`)
+
+    obtenerPersonaje(2, function (personaje) {
+        console.log(`Hola, yo soy ${personaje.name}`)
+
+        obtenerPersonaje(3, function (personaje) {
+            console.log(`Hola, yo soy ${personaje.name}`)
+
+            obtenerPersonaje(4, function (personaje) {
+                console.log(`Hola, yo soy ${personaje.name}`)
+
+                obtenerPersonaje(5, function (personaje) {
+                    console.log(`Hola, yo soy ${personaje.name}`)
+                })
             })
         })
     })
